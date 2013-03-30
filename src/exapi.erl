20349d5f5ebbb6e680ed3478b6658812574027ef
Sergey Abramyan <sa.abramyan@gmail.com>
2013-03-30 13:44:15 +0400

    Move all xmlrpc call in xapi_request and add parse_xapi_request

--- src/exapi.erl
+++ src/exapi.erl
@@ -27,17 +27,37 @@ request(Req, Params) ->
     Result = gen_server:call(?SERVER, {call, Req, Params}),
     Result.
 
+parse_xapi_request(ReqResult) ->
+    Result = case ReqResult of
+                 {ok,{response,[{struct, PreResult}]}} -> PreResult;
+                 _Other -> {error, unknown}
+             end,
+    Result.
+
+xapi_request(Req, Params, State) ->
+    ReqResult = xmlrpc:call(State#sref.host, 80, "/",{call, Req, [State#sref.sref | Params]}),
+
+    Result = case parse_xapi_request(ReqResult) of
+                 {error, unknown} -> error;
+                 PreResult -> case proplists:get_value('Value', PreResult) of
+                                  {array, List} -> List;
+                                  {struct, Struct} -> Struct;
+                                  String -> String
+                              end
+             end,
+    Result.
+
 init([Host, AuthInfo]) ->
-    {ok, {response, [{struct, Result}]}} = xmlrpc:call(Host, 80, "/", {call, session.login_with_password, AuthInfo}),
+    {ok, {response, [{struct, Result}]}} = xmlrpc:call(Host, 80, "/", {call, 'session.login_with_password', AuthInfo}),
     SessionRef = proplists:get_value('Value', Result),
     {ok, #sref{sref = SessionRef, host = Host}}.
 
 handle_call({call, Req}, _From, State) ->
-    {ok,{response,[{struct,[_, {_,{struct, [{_, {struct, Result}}]}}]}]}} = xmlrpc:call(State#sref.host, 80, "/", {call, Req, [State#sref.sref]}),
+    Result = xapi_request(Req, [], State),
     {reply, Result, State};
 
 handle_call({call, Req, Params}, _From, State) ->
-    Result = xmlrpc:call(State#sref.host, 80, "/", {call, Req, [State#sref.sref, Params]}),
+    Result = xapi_request(Req, Params, State),
     {reply, Result, State}.
 
 handle_cast(_Msg, State) ->
